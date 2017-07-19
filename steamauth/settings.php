871b1db8ebeca2768895bc6dffb3502649883e03
ARcases <Argamingaus@gmail.com>
2017-07-19 18:57:31 +1000

    Add files via upload

---
+++ steamauth/settings.php
@@ -0,0 +1,6 @@
+<?php
+$steamauth['apikey'] = "7E4588BEAFDDFC3E275DB57D82CED8A5"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "http://csgo-hell.ru/"; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "/"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+?>
